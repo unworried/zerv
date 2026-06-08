3d152149dfb1f42d197d1f5d788433ce7024dd03
unworried <unwrd@proton.me>
2026-06-07 18:13:24 -0700

    update: impl to issue compile warning for targets other then linux

--- examples/basic.zig
+++ examples/basic.zig
@@ -1,9 +1,37 @@
+comptime {
+    if (@import("builtin").os.tag != .linux) @compileError("Linux only");
+}
+
 const std = @import("std");
 const zerv = @import("zerv");
-const log = std.log.scoped(.main);
+const log = std.log.scoped(.basic);
 
 const LISTEN_ADDR = "127.0.0.1";
 const LISTEN_PORT = 8000;
+
+fn watchShutdown(server: *zerv.Server) void {
+    var set = std.posix.sigemptyset();
+    std.posix.sigaddset(&set, std.posix.SIG.INT);
+    std.posix.sigaddset(&set, std.posix.SIG.TERM);
+
+    const fd = std.posix.signalfd(
+        -1,
+        &set,
+        std.os.linux.SFD.CLOEXEC,
+    ) catch |err| {
+        log.err("signalfd failed: {}", .{err});
+        return;
+    };
+    defer _ = std.os.linux.close(fd);
+
+    var info: std.os.linux.signalfd_siginfo = undefined;
+    _ = std.posix.read(fd, std.mem.asBytes(&info)) catch |err| {
+        log.err("signal read failed: {}", .{err});
+        return;
+    };
+
+    server.shutdown();
+}
 
 fn index(req: *std.http.Server.Request) !void {
     return req.respond("Hello World!", .{ .status = .ok });
@@ -13,24 +41,18 @@ const routes = [_]zerv.Route{
     .{ .method = .GET, .path = "/", .handler = index },
 };
 
-var g_server: *zerv.Server = undefined;
-
-fn onSignal(_: std.posix.SIG) callconv(.c) void {
-    g_server.shutdown();
-}
-
 pub fn main(init: std.process.Init) !void {
     log.info("Starting Server", .{});
     var server = zerv.Server.init(&routes);
 
-    g_server = &server;
-    const sa = std.posix.Sigaction{
-        .handler = .{ .handler = onSignal },
-        .mask = std.posix.sigemptyset(),
-        .flags = 0,
-    };
-    std.posix.sigaction(std.posix.SIG.INT, &sa, null);
-    std.posix.sigaction(std.posix.SIG.TERM, &sa, null);
+    var set = std.posix.sigemptyset();
+    std.posix.sigaddset(&set, std.posix.SIG.INT);
+    std.posix.sigaddset(&set, std.posix.SIG.TERM);
+    std.posix.sigprocmask(std.posix.SIG.BLOCK, &set, null);
+
+    var shutdown_thread = try std.Thread.spawn(.{}, watchShutdown, .{&server});
+    defer shutdown_thread.join();
 
     try server.listen(init.io, LISTEN_ADDR, LISTEN_PORT);
+    log.info("Stopping server", .{});
 }
